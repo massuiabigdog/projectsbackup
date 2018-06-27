2b54fef5fd2aff4f2c8bee59aaf97ec278e8839b
tmassuia <thiago.massuia@hotmail.com>
2018-06-27 00:53:52 -0300

    1
    aaa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,7 +33,35 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
+        $( function() {
+            $( "#research" ).on( "click", function() {
+              $( ".research" ).toggleClass( "hidden", 5000 );
+              $( "#research" ).toggleClass( "actile-tech", 5000 );
+            });
+            $( "#code" ).on( "click", function() {
+                $( ".code" ).toggleClass( "hidden", 5000 );
+                $( "#code" ).toggleClass( "actile-tech", 5000 );
+              });
+          } );
+          $( "#mkt" ).on( "click", function() {
+            $( ".tech-mkt" ).toggleClass( "hidden", 5000 );
+            $( "#mkt" ).toggleClass( "actile-tech", 5000 );
+          });
 
+          $( "#ui" ).on( "click", function() {
+            $( ".tech-ui" ).toggleClass( "hidden", 5000 );
+            $( "#ui" ).toggleClass( "actile-tech", 5000 );
+          });
+
+          $( "#ux" ).on( "click", function() {
+            $( ".tech-ux" ).toggleClass( "hidden", 5000 );
+            $( "#ux" ).toggleClass( "actile-tech", 5000 );
+          });
+
+          $( "#specs" ).on( "click", function() {
+            $( ".tech-specs" ).toggleClass( "hidden", 5000 );
+            $( "#specs" ).toggleClass( "actile-tech", 5000 );
+          });
 
     }
 
