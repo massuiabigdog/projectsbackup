f9be448372a440737912eee7638d1d4e97598609
tmassuia <thiago.massuia@hotmail.com>
2018-07-09 20:34:54 -0300

    responsive

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -32,7 +32,7 @@ export class PostComponent implements OnInit {
 
     ngOnInit() {
         
-       
+       document.getElementsByClassName("ux")
 
         this.route.params.forEach((params: Params) => {
             let id = +params['id'];
@@ -43,6 +43,14 @@ export class PostComponent implements OnInit {
                 r => this.posts = r,
                 error => console.error('Error: ' + error)
             );
+
+
+   
+
+            $( "#code" ).replaceWith( "<h2><i class='material-icons'>code</i>1 heading</h2>" );
+$( ".ux" ).replaceWith( "<h2><i class='material-icons'>code</i>2 heading</h2>" );
+
+$( ".research" ).replaceWith( "<h2><i class='material-icons'>code</i>3 heading</h2>" );
 
         });
 
@@ -92,10 +100,7 @@ $(function(){
         console.log(e);
     });
 });
-$( "_ngcontent-c4.code" ).replaceWith( "<h2><i class='material-icons'>code</i>1 heading</h2>" );
-$( "_ngcontent-c4.ux" ).replaceWith( "<h2><i class='material-icons'>code</i>2 heading</h2>" );
 
-$( "_ngcontent-c4 .research" ).replaceWith( "<h2><i class='material-icons'>code</i>3 heading</h2>" );
 
     }
 
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jquery:any;
+declare var $ :any;
+
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
